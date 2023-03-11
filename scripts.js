ba579be56e55bb0b9edf8c3f352393330d556274
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-11 10:29:15 -0600

    Game logic

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+}
+
+const playGame = () => {
+    const rockButton = document.querySelector('.rock');
+    const paperButton = document.querySelector('.paper');
+    const scissorsButton = document.querySelector('.scissors')
+}
