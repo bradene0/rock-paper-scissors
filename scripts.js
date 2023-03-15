ff9a4d5b2544aa5d4c25c3071b3c90f956146594
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-14 22:51:07 -0500

    Removed Whitespace

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const game = () => {
     let moves = 0;
 
 
-const playGame = () => {
+const playGame = ()=> {
     const rockButton = document.querySelector('.rock');
     const paperButton = document.querySelector('.paper');
     const scissorsButton = document.querySelector('.scissors');
